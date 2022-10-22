f9252184174a9b1817471a14842a5e126760c9d6
GrigoryVelchy <veldog2323@yandex.ru>
2022-10-22 16:25:22 +0300

    polygon+geoJson

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,50 @@
 <template><div id="map-wrap" style="height: 100vh">
   <client-only>
-    <l-map :zoom=13 :center="[53.008545,37.841087]">
-      <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
-      <l-marker :lat-lng="[53.008545,37.841087]"></l-marker>
-      <l-geo-json :geojson="geoJson"  :option-style="geoStyleOption"></l-geo-json>
+    <l-map :zoom=8 :center="centerMap"  style="{width: 100%}" @update:center="writeCenter">
+
+      <l-control>
+        <button @click="topo=!topo">Click to change map</button>
+      </l-control>
+      <l-tile-layer v-if="!topo" url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
+      <l-tile-layer v-else url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"></l-tile-layer>
+
+      <l-marker :lat-lng="[52.6031,39.5708]"></l-marker>
+      <l-polygon color="black" :lat-lngs="latLngsPoligon"></l-polygon>
+      <l-geo-json v-if="false" :geojson="geoJson"  :options-style="geoStyleOption" :options="geoOptions"></l-geo-json>
+
     </l-map>
   </client-only>
 </div>
 </template>
 
 <script>
+
   export default {
     name: 'index',
     data() {
       return {
+        centerMap:[52.6031,39.5708],
         geoJson: null,
-        geoJson1:null,
+        topo:false,
+        maxBounds: [
+          [51.80289371957734, 37.40707869686964],
+          [53.72958979032285, 41.180864748521394]
+        ],
+        latLngsPoligon:[[],[]],
       }
+    },
+    methods:{
+      async getGeoJson(url,latlngPol){
+        const response = await fetch(url)
+        const geoJson = await response.json()
+        if(latlngPol){return geoJson.features[0].geometry.coordinates}
+        let arrOfCoords = geoJson.features[0].geometry.coordinates.map(el=>el.reverse())
+        const newGeoJson = JSON.parse(JSON.stringify(geoJson))
+        newGeoJson.features[0].geometry.coordinates = [[]]
+        newGeoJson.features[0].geometry.coordinates[[0]] = arrOfCoords
+        return newGeoJson
+      },
+      writeCenter(e){console.log(e)}
     },
     computed: {
       geoOptions(){
@@ -26,24 +54,22 @@
       },
       onEachFeatureFunc(){
         return (feature,layer)=>{
-          if(feature.properties.code){
-            layer.bindTooltip(`<div>${feature.properties.code}</div>`, {permanent:true, sticky:"true"})
+          if(feature.properties.id){
+            layer.bindTooltip(`<div>${feature.properties.id}</div>`, {permanent:false, sticky:"true"})
           }
         }
       },
       geoStyleOption(){
-        return{weight:10}
-      }
+          return{
+            weight:3,
+            color:`rgba(0,0,0,0.5)`,
+            fillColor:`rgba(0,0,0,0.3)`
+          }
+      },
     },
     async created() {
-      const response = await fetch("https://gis-api.admlr.lipetsk.ru/api/v1/borders")
-      const response1 = await fetch("https://rawgit.com/gregoiredavid/france-geojson/master/regions/pays-de-la-loire/communes-pays-de-la-loire.geojson")
-      const geoJson = await response.json()
-      const geoJson1 = await response1.json()
-      this.geoJson = geoJson
-      this.geoJson1 = geoJson1
-      console.log(this.geoJson.features[0].geometry.coordinates)
-      console.log(this.geoJson1.features[0].geometry.coordinates)
+      this.latLngsPoligon = await this.getGeoJson("https://gis-api.admlr.lipetsk.ru/api/v1/borders", true)
+      this.geoJson = await this.getGeoJson("https://gis-api.admlr.lipetsk.ru/api/v1/borders", false)
     }
   }
 </script>
